51f6b6fadda09bb994b213a21f696828506ab5f3
AlexanderMihalich <78353628+AlexanderMihalich@users.noreply.github.com>
2021-08-23 19:16:05 +0300

    added gender block

--- src/AppForm.js
+++ src/AppForm.js
@@ -4,8 +4,9 @@ import cn from 'classnames';
 
 function AppForm() {
 	const sendData = (formData) => {
+		console.log(formData);
 		formData.CreatePassword === formData.ConfirmPassword
-			? alert(`login - ${formData.Email}, password - ${formData.CreatePassword}`)
+			? alert(` gender - ${formData.gender}, \n login - ${formData.Email}, \n password - ${formData.CreatePassword}`)
 			: alert("please confirm password")
 	}
 	return (
@@ -13,7 +14,7 @@ function AppForm() {
 			<div className={cn("wrapper__content", "_container")}>
 				<section className="login">
 					<div className="login__logo">
-						<img src={logo} />
+						<img alt="logo" src={logo} />
 					</div>
 					<h1 className="login__title">Sign Up with email</h1>
 					<LoginForm onSubmit={sendData} />
